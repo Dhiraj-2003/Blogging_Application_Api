6c4e6530349d2db4c83c9322438a5e8fe2cc9863
Dhiraj Chaudhary <dhirajchaudhary199@gmail.com>
2024-12-28 22:57:11 +0530

    JWT-Authentication-added

--- src/main/java/com/dhiraj/blog/config/SecurityConfig.java
+++ src/main/java/com/dhiraj/blog/config/SecurityConfig.java
@@ -1,27 +1,82 @@
 package com.dhiraj.blog.config;
 
 import com.dhiraj.blog.security.CustomUserDetailService;
+import com.dhiraj.blog.security.JwtAuthenticationEntryPoint;
+import com.dhiraj.blog.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class SecurityConfig {
 
     @Autowired
     private CustomUserDetailService customUserDetailService;
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        httpSecurity.authenticationProvider(daoAuthenticationProvider());
+        DefaultSecurityFilterChain defaultSecurityFilterChain = httpSecurity.build();
+        return defaultSecurityFilterChain;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.customUserDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfig) throws Exception{
+        return authenticationConfig.getAuthenticationManager();
     }
 }
---
+++ src/main/java/com/dhiraj/blog/controllers/AuthController.java
@@ -0,0 +1,60 @@
+package com.dhiraj.blog.controllers;
+
+import com.dhiraj.blog.payloads.JwtAuthRequest;
+import com.dhiraj.blog.payloads.JwtAuthResponse;
+import com.dhiraj.blog.security.JwtTokenHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    private JwtTokenHelper jwtTokenHelper;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> createToken(
+            @RequestBody JwtAuthRequest request
+    ) throws Exception {
+        System.out.println(request.getUsername());
+        System.out.println(request.getPassword());
+        this.authenticate(request.getUsername(),request.getPassword());
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
+        String generatedToken = this.jwtTokenHelper.generateToken(userDetails);
+        JwtAuthResponse authResponse = new JwtAuthResponse();
+        authResponse.setToken(generatedToken);
+        return new ResponseEntity<JwtAuthResponse>(authResponse, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password) throws Exception{
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username,password);
+        try{
+            this.authenticationManager.authenticate(token);
+        }catch (BadCredentialsException e){
+            System.out.println("Invalid User Details !!!");
+            throw new Exception("Invalid User name or password !!!");
+        }
+
+
+    }
+}
