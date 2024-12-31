e7369752780539a212dbf271c84f5c55c62d3800
Dhiraj Chaudhary <dhirajchaudhary199@gmail.com>
2024-12-31 18:49:06 +0530

    Register_User_api

--- src/main/java/com/dhiraj/blog/payloads/UserDto.java
+++ src/main/java/com/dhiraj/blog/payloads/UserDto.java
@@ -7,12 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
-
-    private Integer id;
 
     @NotEmpty
     @Size(min = 2,message = "username must be of min 2 characters !!!")
@@ -27,4 +28,6 @@ public class UserDto {
 
     @NotEmpty
     private String about;
+
+    private Set<RoleDto> roles = new HashSet<>();
 }
